cadfed81beb7e34b7f1b4bb88a2e8896ae0af0df
IBrickhouse <mrfurbyb@gmail.com>
2022-07-04 20:43:29 -0700

    Change Audio Playing Strategy
    Change audio playing strategy and move on to start visual file.

---
+++ musicPlayer.py
@@ -0,0 +1,19 @@
+# import vlc
+
+# player = vlc.MediaPlayer()
+
+# media = vlc.MediaPlayer("asian-uplifting-corporate-71088.mp3")
+
+# player.set_media(song)
+
+# media.play()
+
+# Start Over witout vlc
+import webbrowser
+#import playsound
+
+
+#webbrowser.open("http://google.com")
+
+webbrowser.open("asian-uplifting-corporate-71088.mp3")
+#webbrowser.open("/Users/isaiahbrickhouse/Git Repos/Music Visualizer/asian-uplifting-corporate-71088.mp3")
