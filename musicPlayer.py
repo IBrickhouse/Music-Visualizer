2891d1c9904ff673db0c10e3f1b6a13bf87436cb
IBrickhouse <mrfurbyb@gmail.com>
2022-07-06 17:10:20 -0700

    Switch To Native Audio Player
    Switch to native audio player because I can get ot to work. May not be final solution.

--- musicPlayer.py
+++ musicPlayer.py
@@ -1,19 +1,6 @@
-# import vlc
-
-# player = vlc.MediaPlayer()
-
-# media = vlc.MediaPlayer("asian-uplifting-corporate-71088.mp3")
-
-# player.set_media(song)
-
-# media.play()
-
-# Start Over witout vlc
-import webbrowser
-#import playsound
-
-
-#webbrowser.open("http://google.com")
-
-webbrowser.open("asian-uplifting-corporate-71088.mp3")
-#webbrowser.open("/Users/isaiahbrickhouse/Git Repos/Music Visualizer/asian-uplifting-corporate-71088.mp3")
+import os
+  
+# play sound
+file = "asian-uplifting-corporate-71088.mp3"
+print('playing sound using native player')
+os.system("afplay " + file)
